5d3810fa5658de222785391ffe816dd8497cf860
jacobcoleman <jcoleman414@gmail.com>
2015-02-19 19:23:13 -0500

    Updated labels as captions

---
+++ Test3/src/edu/utc/atc/widgetset/client/googlemapswidget/GoogleMapsWidgetState.java
@@ -0,0 +1,8 @@
+package edu.utc.atc.widgetset.client.googlemapswidget;
+
+public class GoogleMapsWidgetState extends com.vaadin.shared.AbstractComponentState {
+
+	// TODO example state
+	public String text = "This is GoogleMapsWidget";
+
+}
---
+++ Test3/src/edu/utc/atc/widgetset/client/googlemapswidget/GoogleMapsWidgetWidget.java
@@ -0,0 +1,17 @@
+package edu.utc.atc.widgetset.client.googlemapswidget;
+
+import com.google.gwt.user.client.ui.Label;
+
+// TODO extend any GWT Widget
+public class GoogleMapsWidgetWidget extends Label {
+
+	public static final String CLASSNAME = "googlemapswidget";
+
+	public GoogleMapsWidgetWidget() {
+
+		// setText("GoogleMapsWidget sets the text via GoogleMapsWidgetConnector using GoogleMapsWidgetState");
+		setStyleName(CLASSNAME);
+
+	}
+
+}
