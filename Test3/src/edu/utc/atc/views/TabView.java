1b00ef7682cb4abd394b84e715e86bd811c68add
jacobcoleman <jcoleman414@gmail.com>
2015-02-24 17:39:57 -0500

    Began work on collecting users timezone from the browser

--- Test3/src/edu/utc/atc/views/TabView.java
+++ Test3/src/edu/utc/atc/views/TabView.java
@@ -2,6 +2,16 @@ package edu.utc.atc.views;
 
 
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import com.vaadin.server.WebBrowser;
+
+import java.util.TimeZone;
+
+import com.vaadin.server.Page;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.TabSheet.SelectedTabChangeEvent;
 
@@ -12,9 +22,15 @@ public class TabView extends TabComponent{
 	public static double longitude;
 	public static double distance;
 	public static double depth;
+
+	public static TimeZone usersTimeZone;
+	
+	
+	private int offset;
 	private QueryView qv;
 	private TimeCalcView tcv;
 	private GoogleMapView gm;
+	
 	
 	
 	/**
@@ -24,16 +40,36 @@ public class TabView extends TabComponent{
 
 	public TabView ()
 	{
-		
 		this.tabSheet_MainTab.addSelectedTabChangeListener(new TabSheet.SelectedTabChangeListener() {
 			public void selectedTabChange(SelectedTabChangeEvent event) {
+				
+				
+				
 				
 			   //Sets properties in TimeCalcView component and updates each time the ATC view is clicked in the tab
 	           tcv.setDistance(distance);
 	           tcv.setDepth(depth);
 	           tcv.setLatitude(latitude);
 	           tcv.setLongitude(longitude);
-
+	   
+	           WebBrowser browser = (WebBrowser) Page.getCurrent().getWebBrowser();
+	           offset = browser.getTimezoneOffset();
+	           System.out.println(offset);
+	           /**
+	           usersTimeZone.setRawOffset(offset);;
+	           
+	           Calendar cal = Calendar.getInstance();
+	   		   TimeZone serverTimeZone = cal.getTimeZone();
+	   		
+	   		   cal.setTimeZone(serverTimeZone);
+	   		   cal.add(Calendar.MILLISECOND, getUI().getPage().getWebBrowser().getTimezoneOffset() * - 1);
+	   		   System.out.println(cal.toString());
+	           
+	           Notification distanceError = new Notification(usersTimeZone.getID(),Notification.TYPE_WARNING_MESSAGE);
+				distanceError.show(Page.getCurrent());
+	   		   //usersTimeZone.getOffset(browser.getRawTimezoneOffset());
+	            **/
+				
 			}
 	        });
 		
@@ -43,8 +79,10 @@ public class TabView extends TabComponent{
 		//Adds View to the second tab on the tab sheet
 		tcv = new TimeCalcView();
 		tabSheet_Tab2.addComponent(tcv);
-		gm = new GoogleMapView();
-		tabSheet_Tab3.addComponent(gm);
+		//gm = new GoogleMapView();
+		//tabSheet_Tab3.addComponent(gm);
+		
+		
 		
 	}
 }
